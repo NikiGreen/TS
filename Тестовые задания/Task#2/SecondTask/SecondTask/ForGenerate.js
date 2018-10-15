d36e5e582e002900359bb3ddda3724d43e21016b
NikiGreen <antimagantimagov@mail.ru>
2018-10-15 15:50:05 +0300

    Bug fix+auto generate table elements

---
+++ Тестовые задания/Task#2/SecondTask/SecondTask/ForGenerate.js
@@ -0,0 +1,7 @@
+function main_table() {
+    var i;
+        for(i=11;i>=-12;i--){
+        var o=document.createElement('th')
+        o.innerHTML = i;
+        document.getElementById('for_generate').appendChild(o)}
+}
--- Тестовые задания/Task#2/SecondTask/SecondTask/SecondTaskScript.js
+++ Тестовые задания/Task#2/SecondTask/SecondTask/SecondTaskScript.js
@@ -1,31 +1,42 @@
 var speed=0;
+var position=0;
 
 function A() {
     if (speed <= 0)
         speed = 1;
     else
         speed = speed * 2;
-
     var image=document.getElementById("object");
-    /*image.style.left=image.offsetLeft+55;
-var hi= image.getBoundingClientRect().left;*/
     image.style.position = "absolute";
-    if(image.getBoundingClientRect().left-(55.75*speed)>290)
-    var x=image.getBoundingClientRect().left-(55.75*speed);else
+    if(image.getBoundingClientRect().left-(55.75*speed)>290){
+        position+=speed;
+        var x=image.getBoundingClientRect().left-(55.75*speed);
+    } else {
         alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам сбросить скорость или вернутся назад!!!");
+
+       /* var prompt  =   document.getElementById('for_generate');
+        prompt.innerHTML = "<th>\"1\"</th>";*/
+
+
+        /*var i;
+        for(i=11;i>=-12;i--){
+            var o=document.createElement('th')
+        o.innerHTML = i;
+        document.getElementById('main_table').appendChild(o)}*/
+    }
     image.style.left=x+"px";
     Speed(speed);
-
 }
 
 function R() {
     speed = -1;
+
     var image=document.getElementById("object");
-    /*image.style.left=image.offsetLeft+55;
-var hi= image.getBoundingClientRect().left;*/
     image.style.position = "absolute";
-    if(image.getBoundingClientRect().left+55.75*Math.abs(speed)<1600)
-    var y=image.getBoundingClientRect().left+55.75*Math.abs(speed);else
+    if(image.getBoundingClientRect().left+55.75*Math.abs(speed)<1600){
+        position+=speed;
+        var y=image.getBoundingClientRect().left+55.75*Math.abs(speed);
+    } else
         alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам начать движение вперёд!!!");
     image.style.left=y+"px";
     Speed(speed);
@@ -37,7 +48,8 @@ function Speed(speed) {
 
 function Position() {
     var pos=document.getElementById("object");
-    document.getElementById("Position").value="Позиция ="+pos.getBoundingClientRect().left+"px";
+    // document.getElementById("Position").value="Позиция ="+pos.getBoundingClientRect().left+"px";
+    document.getElementById("Position").value="Позиция ="+position;
 }
 
 function forA() {
