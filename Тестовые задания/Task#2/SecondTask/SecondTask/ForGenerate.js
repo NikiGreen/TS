1f585026cc0158451ad148432a75aa98089ef3f1
NikiGreen <antimagantimagov@mail.ru>
2018-10-15 17:14:37 +0300

    problem of value generation is solved

--- Тестовые задания/Task#2/SecondTask/SecondTask/ForGenerate.js
+++ Тестовые задания/Task#2/SecondTask/SecondTask/ForGenerate.js
@@ -1,7 +1,7 @@
 function main_table() {
     var i;
-        for(i=11;i>=-12;i--){
-        var o=document.createElement('th')
+        for(i=11;i>=-11;i--){
+        var o=document.createElement('th');
         o.innerHTML = i;
-        document.getElementById('for_generate').appendChild(o)}
+        document.getElementById('for_generate').appendChild(o);}
 }
--- Тестовые задания/Task#2/SecondTask/SecondTask/SecondTaskScript.js
+++ Тестовые задания/Task#2/SecondTask/SecondTask/SecondTaskScript.js
@@ -1,5 +1,7 @@
 var speed=0;
 var position=0;
+var i1=11;
+var i2=-13;
 
 function A() {
     if (speed <= 0)
@@ -8,21 +10,14 @@ function A() {
         speed = speed * 2;
     var image=document.getElementById("object");
     image.style.position = "absolute";
-    if(image.getBoundingClientRect().left-(55.75*speed)>290){
+    if(image.getBoundingClientRect().left-(58*speed)>290){
         position+=speed;
-        var x=image.getBoundingClientRect().left-(55.75*speed);
+        var x=image.getBoundingClientRect().left-(57.5*speed);
     } else {
-        alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам сбросить скорость или вернутся назад!!!");
-
-       /* var prompt  =   document.getElementById('for_generate');
-        prompt.innerHTML = "<th>\"1\"</th>";*/
-
-
-        /*var i;
-        for(i=11;i>=-12;i--){
-            var o=document.createElement('th')
-        o.innerHTML = i;
-        document.getElementById('main_table').appendChild(o)}*/
+        /*alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам сбросить скорость или вернутся назад!!!");*/
+        document.getElementById('for_generate' ).remove();
+        area_plus();
+        position+=speed;
     }
     image.style.left=x+"px";
     Speed(speed);
@@ -33,11 +28,15 @@ function R() {
 
     var image=document.getElementById("object");
     image.style.position = "absolute";
-    if(image.getBoundingClientRect().left+55.75*Math.abs(speed)<1600){
+    if(image.getBoundingClientRect().left+58.56*Math.abs(speed)<1600){
         position+=speed;
-        var y=image.getBoundingClientRect().left+55.75*Math.abs(speed);
-    } else
+        var y=image.getBoundingClientRect().left+58.56*Math.abs(speed);
+    } else {
         alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам начать движение вперёд!!!");
+        document.getElementById('for_generate' ).remove();
+        area_minus();
+        position+=speed;
+    }
     image.style.left=y+"px";
     Speed(speed);
 }
@@ -60,4 +59,28 @@ function forA() {
 function forR() {
     R();
     Position();
+}
+
+function area_plus() {
+    var em1=document.createElement('tr');
+    em1.id="for_generate";
+    var i;
+    for(i=i1+23;i>=i1+1;i--){
+        var em2=document.createElement('th');
+        em2.innerHTML = i;
+        document.getElementById('main_table').appendChild(em1);
+        document.getElementById('for_generate').appendChild(em2);}
+    i1+=23;
+}
+function area_minus() {
+    i1-=23;
+    var em1=document.createElement('tr');
+    em1.id="for_generate";
+    var i;
+    for(i=i1;i>=i1-22;i--){
+        var em2=document.createElement('th');
+        em2.innerHTML = i;
+        document.getElementById('main_table').appendChild(em1);
+        document.getElementById('for_generate').appendChild(em2);}
+
 }
