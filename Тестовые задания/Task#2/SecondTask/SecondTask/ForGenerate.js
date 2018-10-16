5013651b43bacb226570fdcb16d3e78377568456
NikiGreen <antimagantimagov@mail.ru>
2018-10-16 13:46:18 +0300

    Task#2 is Complete

--- Тестовые задания/Task#2/SecondTask/SecondTask/ForGenerate.js
+++ Тестовые задания/Task#2/SecondTask/SecondTask/ForGenerate.js
@@ -1,7 +1,12 @@
-function main_table() {
+function main_table() {/*Функция создания таблицы при загрузке страницы*/
     var i;
         for(i=11;i>=-11;i--){
         var o=document.createElement('th');
         o.innerHTML = i;
+        o.id=i;
         document.getElementById('for_generate').appendChild(o);}
+    var ner=document.getElementById(position).getBoundingClientRect();
+    var image=document.getElementById("object");
+    image.style.left=ner.left+"px";image.style.right=ner.right+"px";image.style.top=ner.top+"px";image.style.bottom=ner.bottom+"px";
+
 }
--- Тестовые задания/Task#2/SecondTask/SecondTask/SecondTaskScript.js
+++ Тестовые задания/Task#2/SecondTask/SecondTask/SecondTaskScript.js
@@ -1,53 +1,46 @@
 var speed=0;
 var position=0;
 var i1=11;
-var i2=-13;
 
-function A() {
+function A() {/*Функция перемещения вперёд*/
     if (speed <= 0)
         speed = 1;
     else
         speed = speed * 2;
+    position+=speed;
     var image=document.getElementById("object");
     image.style.position = "absolute";
-    if(image.getBoundingClientRect().left-(70*speed)>290){
-        position+=speed;
-        var x=image.getBoundingClientRect().left-(70*speed);
+        if(position<=i1){
+
+        var ner=document.getElementById(position).getBoundingClientRect();
+
+        image.style.left=ner.left+"px";image.style.right=ner.right+"px";image.style.top=ner.top+"px";image.style.bottom=ner.bottom+"px";
     } else {
-        /*alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам сбросить скорость или вернутся назад!!!");*/
         document.getElementById('for_generate' ).remove();
         area_plus();
-        position+=speed;
-    }
-    image.style.left=x+"px";
-    Speed(speed);
+    }Speed(speed);
 }
 
-function R() {
+function R() {/*Функция перемещения назад*/
     speed = -1;
-
+    position+=speed;
     var image=document.getElementById("object");
     image.style.position = "absolute";
-    if(image.getBoundingClientRect().left+43*Math.abs(speed)<1600){
-        position+=speed;
-        var y=image.getBoundingClientRect().left+43*Math.abs(speed);
+    if(position>i1-23){
+        var ner=document.getElementById(position).getBoundingClientRect();
+
+        image.style.left=ner.left+"px";image.style.right=ner.right+"px";image.style.top=ner.top+"px";image.style.bottom=ner.bottom+"px";
     } else {
-        alert("Вы пытаетесь покинуть зону пригодную для перемещения марсохода!!! \nРекомендуем вам начать движение вперёд!!!");
         document.getElementById('for_generate' ).remove();
         area_minus();
-        position+=speed;
-    }
-    image.style.left=y+"px";
-    Speed(speed);
+    }Speed(speed);
 }
 
-function Speed(speed) {
+function Speed(speed) {/*Функция отображения скорости*/
     document.getElementById("Speed").value="Скорость= "+speed;
 }
 
-function Position() {
-    var pos=document.getElementById("object");
-    // document.getElementById("Position").value="Позиция ="+pos.getBoundingClientRect().left+"px";
+function Position() {/*Функция отображения позиции*/
     document.getElementById("Position").value="Позиция ="+position;
 }
 
@@ -61,26 +54,39 @@ function forR() {
     Position();
 }
 
-function area_plus() {
+function area_plus() {/*Функция генерации поля при выходе за границы в сторону+*/
+    while(position>i1+23)
+    i1+=23;
     var em1=document.createElement('tr');
     em1.id="for_generate";
     var i;
     for(i=i1+23;i>=i1+1;i--){
         var em2=document.createElement('th');
         em2.innerHTML = i;
+        em2.id=i;
         document.getElementById('main_table').appendChild(em1);
         document.getElementById('for_generate').appendChild(em2);}
+    var ner=document.getElementById(position).getBoundingClientRect();
+    var image=document.getElementById("object");
+    image.style.left=ner.left+"px";image.style.right=ner.right+"px";image.style.top=ner.top+"px";image.style.bottom=ner.bottom+"px";
+    image.width=ner.offsetWidth;
+    image.height=ner.offsetHeight;
     i1+=23;
 }
-function area_minus() {
-    i1-=23;
+function area_minus() {/*Функция генерации поля при выходе за границы в сторону-*/
     var em1=document.createElement('tr');
     em1.id="for_generate";
     var i;
-    for(i=i1;i>=i1-22;i--){
+    for(i=i1-23;i>=i1-45;i--){
         var em2=document.createElement('th');
         em2.innerHTML = i;
+        em2.id=i;
         document.getElementById('main_table').appendChild(em1);
         document.getElementById('for_generate').appendChild(em2);}
-
+    var ner=document.getElementById(position).getBoundingClientRect();
+    var image=document.getElementById("object");
+    image.style.left=ner.left+"px";image.style.right=ner.right+"px";image.style.top=ner.top+"px";image.style.bottom=ner.bottom+"px";
+    image.width=ner.offsetWidth;
+    image.height=ner.offsetHeight;
+    i1-=23;
 }
